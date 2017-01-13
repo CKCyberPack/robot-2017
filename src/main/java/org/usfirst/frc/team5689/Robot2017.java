bd3c90d07f5182ac6004a9010f905d6c64c28b4b
Brian Boundy <github@brianboundy.ca>
2017-01-13 09:16:06 -0500

    2017 Main Class methods and Maven depdencies updated to only JAVA sources. READY FOR DEVELOPMENT.

--- src/main/java/org/usfirst/frc/team5689/Robot2017.java
+++ src/main/java/org/usfirst/frc/team5689/Robot2017.java
@@ -1,4 +1,45 @@
 package org.usfirst.frc.team5689;
 
-public class Robot2017 {
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot2017 extends IterativeRobot {
+    @Override
+    public void robotInit() {
+        super.robotInit();
+    }
+
+    @Override
+    public void robotPeriodic() {
+        super.robotPeriodic();
+    }
+
+    @Override
+    public void teleopInit() {
+        super.teleopInit();
+    }
+
+    @Override
+    public void teleopPeriodic() {
+        super.teleopPeriodic();
+    }
+
+    @Override
+    public void autonomousInit() {
+        super.autonomousInit();
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+        super.autonomousPeriodic();
+    }
+
+    @Override
+    public void testInit() {
+        super.testInit();
+    }
+
+    @Override
+    public void testPeriodic() {
+        super.testPeriodic();
+    }
 }
