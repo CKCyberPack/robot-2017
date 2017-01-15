80fc86089bedc4d0ca64c186a72135ceb6f02e2f
auti177 <austin.formagin117@gmail.com>
2017-01-15 11:01:22 -0500

    Austin - Moving robot + Example Gyro

--- src/main/java/org/usfirst/frc/team5689/Robot2017.java
+++ src/main/java/org/usfirst/frc/team5689/Robot2017.java
@@ -1,11 +1,37 @@
 package org.usfirst.frc.team5689;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot2017 extends IterativeRobot {
+    final String defaultAuto = "Default";
+    final String customAuto = "My Auto";
+    String autoSelected;
+    SendableChooser<String> chooser = new SendableChooser<>();
+
+    VictorSP leftMotor;
+    VictorSP rightMotor;
+    XboxController ckController;
+    RobotDrive ckDrive;
+    ADXRS450_Gyro ckGyro;
+
+    boolean bTriggerPressed = false;
+
+
     @Override
     public void robotInit() {
-        super.robotInit();
+        chooser.addDefault("Default Auto", defaultAuto);
+        chooser.addObject("My Auto", customAuto);
+        SmartDashboard.putData("Auto choices", chooser);
+
+
+        leftMotor = new VictorSP(0);
+        rightMotor = new VictorSP(1);
+        ckController = new XboxController(0);
+        ckDrive = new RobotDrive(leftMotor, rightMotor);
+        ckGyro = new ADXRS450_Gyro();
+        ckGyro.calibrate();
     }
 
     @Override
@@ -20,22 +46,75 @@ public class Robot2017 extends IterativeRobot {
 
     @Override
     public void teleopPeriodic() {
-        super.teleopPeriodic();
+
+        SmartDashboard.putNumber("Gyro", ckGyro.getAngle());
+        SmartDashboard.putNumber("Left Motor", leftMotor.getSpeed());
+        SmartDashboard.putNumber("Right Motor", rightMotor.getSpeed());
+
+        if (ckController.getAButton()) {
+            if (bTriggerPressed == false) {
+                //First loop
+                ckGyro.reset();
+            }
+
+            if (ckGyro.getAngle() < 80) {
+                ckDrive.arcadeDrive(0, -0.5);
+            } else if (ckGyro.getAngle() < 89) {
+                ckDrive.arcadeDrive(0, -0.33);
+
+            } else if (ckGyro.getAngle() > 100) {
+                ckDrive.arcadeDrive(0, 0.5);
+            } else if (ckGyro.getAngle() > 91) {
+                ckDrive.arcadeDrive(0, 0.33);
+            } else {
+                ckDrive.stopMotor();
+            }
+
+            bTriggerPressed = true;
+        } else if (ckController.getXButton()) {
+            if (bTriggerPressed == false) {
+                //First loop
+                ckGyro.reset();
+            }
+            //TODO - Use the built in accelerometer to sense a collision and stop
+            ckDrive.arcadeDrive(0.4, ckGyro.getAngle() / 20);
+            bTriggerPressed = true;
+        } else {
+            ckDrive.tankDrive(-ckController.getY(GenericHID.Hand.kLeft), -ckController.getY(GenericHID.Hand.kRight));
+            bTriggerPressed = false;
+        }
     }
 
     @Override
     public void autonomousInit() {
-        super.autonomousInit();
+        autoSelected = chooser.getSelected();
+        // autoSelected = SmartDashboard.getString("Auto Selector",
+        // defaultAuto);
+        System.out.println("Auto selected: " + autoSelected);
     }
 
     @Override
     public void autonomousPeriodic() {
-        super.autonomousPeriodic();
+
+        switch (autoSelected) {
+            case customAuto:
+                // Put custom auto code here
+                break;
+            case defaultAuto:
+            default:
+                // Put default auto code here
+                break;
+        }
+
     }
 
     @Override
     public void testInit() {
         super.testInit();
+
+        VictorSP testing = new VictorSP(2);
+
+
     }
 
     @Override
