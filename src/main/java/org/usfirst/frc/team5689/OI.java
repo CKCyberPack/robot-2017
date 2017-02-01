34e58d9a2c934ef8bebbdcdd398a51dabc0058cd
Paul Norton <napkinorton@gmail.com>
2017-02-01 17:41:52 -0500

    Testing vision robot

--- src/main/java/org/usfirst/frc/team5689/OI.java
+++
@@ -1,9 +0,0 @@
-package org.usfirst.frc.team5689;
-
-import edu.wpi.first.wpilibj.XboxController;
-
-public abstract class OI {
-
-    public static XboxController driveController = new XboxController(0);
-
-}
--- src/main/java/org/usfirst/frc/team5689/Robot2017.java
+++ src/main/java/org/usfirst/frc/team5689/Robot2017.java
@@ -1,178 +1,39 @@
 package org.usfirst.frc.team5689;
 
-import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.first.wpilibj.*;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.cscore.MjpegServer;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.vision.VisionThread;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.imgproc.Imgproc;
 
 public class Robot2017 extends IterativeRobot {
 
-    public static Robot2017 instance;
+    public static final int VISION_WIDTH = 320;
+    public static final int VISION_HEIGHT = 240;
 
-    final String defaultAuto = "Default";
-    final String customAuto = "My Auto";
-    String autoSelected;
-    SendableChooser<String> chooser = new SendableChooser<>();
+    private VisionThread visionThread;
+    private double centerX = 0;
+    private final Object imgLock = new Object();
 
-    BuiltInAccelerometer ckAcc;
-
-    double maxX = 0;
-    double maxY = 0;
-    double maxZ = 0;
-
-    VictorSP leftMotor;
-    VictorSP rightMotor;
-    XboxController ckController;
-    RobotDrive ckDrive;
-    ADXRS450_Gyro ckGyro;
-    Encoder leftEncoder;
-    Encoder rightEncoder;
-    AHRS ckNavX;
-    boolean bTriggerPressed = false;
-    private boolean wallCollision;
-
-    @Override
     public void robotInit() {
-        instance = this;
-        chooser.addDefault("Default Auto", defaultAuto);
-        chooser.addObject("My Auto", customAuto);
-        SmartDashboard.putData("Auto choices", chooser);
+        UsbCamera cam = CameraServer.getInstance().startAutomaticCapture();
+        cam.setResolution(VISION_WIDTH, VISION_HEIGHT);
+        cam.setFPS(2);
 
-
-        leftMotor = new VictorSP(0);
-        rightMotor = new VictorSP(1);
-        ckController = new XboxController(0);
-        ckDrive = new RobotDrive(leftMotor, rightMotor);
-        ckGyro = new ADXRS450_Gyro();
-        ckNavX = new AHRS(SerialPort.Port.kUSB);
-        ckAcc = new BuiltInAccelerometer();
-        leftEncoder = new Encoder(0,1,true);
-        rightEncoder = new Encoder(2,3);
-        leftEncoder.setDistancePerPulse(6 * Math.PI / 360);
-        rightEncoder.setDistancePerPulse(6 * Math.PI / 360);
-    }
-
-    @Override
-    public void robotPeriodic() {
-        super.robotPeriodic();
-    }
-
-    @Override
-    public void teleopInit() {
-        super.teleopInit();
-    }
-
-    @Override
-    public void teleopPeriodic() {
-
-        SmartDashboard.putNumber("Gyro", ckGyro.getAngle());
-        SmartDashboard.putNumber("Left Motor", leftMotor.getSpeed());
-        SmartDashboard.putNumber("Right Motor", rightMotor.getSpeed());
-        SmartDashboard.putNumber("Left Encoder", leftEncoder.getDistance());
-        SmartDashboard.putNumber("Right Encoder", rightEncoder.getDistance());
-        SmartDashboard.putNumber("NavX", ckNavX.getYaw());
-        SmartDashboard.putBoolean("NavX Connected", ckNavX.isConnected());
-
-
-        double cx = ckAcc.getX();
-        double cy = ckAcc.getX();
-        double cz = ckAcc.getX();
-
-        if (cx < maxX)
-            maxX = cx;
-        if (cy < maxY)
-            maxY = cy;
-        if (cz < maxZ)
-            maxZ = cz;
-
-        SmartDashboard.putNumber("Accelerometer X", maxX);
-        SmartDashboard.putNumber("Accelerometer Y", maxY);
-        SmartDashboard.putNumber("Accelerometer Z", maxZ);
-
-
-        if (ckController.getYButton()){
-            leftEncoder.reset();
-            rightEncoder.reset();
-            ckGyro.reset();
-            ckNavX.reset();
-        }
-
-        if (wallCollision && !ckController.getXButton()) wallCollision = false;
-
-        if (ckController.getAButton()) {
-            if (!bTriggerPressed) {
-                //First loop
-                ckGyro.reset();
+        visionThread = new VisionThread(cam, new GripPipeline(), pipeline -> {
+            for (MatOfPoint p : pipeline.filterContoursOutput()) {
+                Rect r = Imgproc.boundingRect(p);
+                SmartDashboard.putNumber("X: ", r.x);
+                SmartDashboard.putNumber("Y: ", r.y);
+                SmartDashboard.putNumber("Width: ", r.width);
+                SmartDashboard.putNumber("Height: ", r.height);
             }
-
-            if (ckGyro.getAngle() < 80) {
-                ckDrive.arcadeDrive(0, -0.5);
-            } else if (ckGyro.getAngle() < 89) {
-                ckDrive.arcadeDrive(0, -0.33);
-
-            } else if (ckGyro.getAngle() > 100) {
-                ckDrive.arcadeDrive(0, 0.5);
-            } else if (ckGyro.getAngle() > 91) {
-                ckDrive.arcadeDrive(0, 0.33);
-            } else {
-                ckDrive.stopMotor();
-            }
-
-            bTriggerPressed = true;
-        } else if (ckController.getXButton() && !wallCollision) {
-            if (!bTriggerPressed) {
-                //First loop
-                ckGyro.reset();
-                wallCollision = false;
-            }
-            //TODO - Use the built in accelerometer to sense a collision and stop
-            ckDrive.arcadeDrive(0.75, ckGyro.getAngle() / 20);
-            bTriggerPressed = true;
-            if (ckAcc.getY() < -1.5) {
-                wallCollision = true;
-            }
-        } else {
-            ckDrive.arcadeDrive(-ckController.getY(GenericHID.Hand.kLeft), -ckController.getX(GenericHID.Hand.kRight));
-            bTriggerPressed = false;
-        }
+        });
+        visionThread.start();
     }
 
-    @Override
-    public void autonomousInit() {
-        autoSelected = chooser.getSelected();
-        // autoSelected = SmartDashboard.getString("Auto Selector",
-        // defaultAuto);
-        System.out.println("Auto selected: " + autoSelected);
-    }
-
-    @Override
-    public void autonomousPeriodic() {
-
-        switch (autoSelected) {
-            case customAuto:
-                // Put custom auto code here
-                break;
-            case defaultAuto:
-            default:
-                // Put default auto code here
-                break;
-        }
-
-    }
-
-    @Override
-    public void testInit() {
-        super.testInit();
-
-        //VictorSP testing = new VictorSP(2);
-
-
-    }
-
-    @Override
-    public void testPeriodic() {
-        super.testPeriodic();
-        LiveWindow.run();
-    }
 }
