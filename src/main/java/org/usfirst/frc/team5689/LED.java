a24065b06b9432f059a6b4a80471948488ed4e63
Brian Boundy <github@brianboundy.ca>
2017-02-15 19:52:37 -0500

    Red and Blue LED

--- src/main/java/org/usfirst/frc/team5689/LED.java
+++ src/main/java/org/usfirst/frc/team5689/LED.java
@@ -4,19 +4,26 @@ import edu.wpi.first.wpilibj.Solenoid;
 
 public class LED {
     private Solenoid visionLED;
+    private Solenoid redLED;
+    private Solenoid blueLED;
     private boolean visionStatus;
+    private boolean redStatus;
+    private boolean blueStatus;
 
     public LED() {
         visionLED = new Solenoid(RobotMap.pcmLEDCamera);
+        redLED = new Solenoid(RobotMap.pcmLEDRed);
+        blueLED = new Solenoid(RobotMap.pcmLEDBlue);
         visionStatus = false;
+        redStatus = false;
+        blueStatus = false;
     }
 
+    //**** Vision LED
     public void visionOn() {
         visionLED.set(true);
         visionStatus = true;
     }
-
-    ;
 
     public void visionOff() {
         visionLED.set(false);
@@ -30,6 +37,46 @@ public class LED {
             visionOn();
         }
     }
+
+    //**** Red LED
+    public void redOn() {
+        redLED.set(true);
+        redStatus = true;
+    }
+
+    public void redOff() {
+        redLED.set(false);
+        redStatus = false;
+    }
+
+    public void toggleRed() {
+        if (redStatus) {
+            redOff();
+        } else {
+            redOn();
+        }
+    }
+
+    //**** Blue LED
+    public void blueOn() {
+        blueLED.set(true);
+        blueStatus = true;
+    }
+
+    public void blueOff() {
+        blueLED.set(false);
+        blueStatus = false;
+    }
+
+    public void toggleBlue() {
+        if (blueStatus) {
+            blueOff();
+        } else {
+            blueOn();
+        }
+    }
+
+
 }
 
 
